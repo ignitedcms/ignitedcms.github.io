9623652303c6c521f994ec6b100e157333c8bf0c
ignitedcms <iamthwee@gmail.com>
2025-02-27 20:57:32 +0000

    chore: docs updates

--- resources/assets/js/hover-card.js
+++ resources/assets/js/hover-card.js
@@ -1,12 +1,12 @@
 /*
 |---------------------------------------------------------------
-| Hovercard  component
+| Hovercard component
 |---------------------------------------------------------------
 |
 |
 | @author: IgnitedCMS
 | @license: MIT
-| @version: 1.0
+| @version: 1.1
 | @since: 1.0
 |
 */
@@ -23,6 +23,26 @@ Vue.component('hovercard', {
       hovercard: this
     };
   },
+  props: {
+    position: {
+      type: String,
+      default: 'bottom',
+      validator: value => ['top', 'bottom', 'left', 'right'].includes(value)
+    },
+    align: {
+      type: String,
+      default: 'center',
+      validator: value => ['start', 'center', 'end'].includes(value)
+    },
+    offset: {
+      type: String,
+      default: '0px'
+    },
+    gap: {
+      type: String,
+      default: '0px'
+    }
+  },
   data() {
     return {
       isOpen: false,
@@ -31,8 +51,29 @@ Vue.component('hovercard', {
       uniqueId: Math.random().toString(36).substring(2)
     };
   },
+  created() {
+    // Create a global event listener for the escape key
+    window.addEventListener('keydown', this.handleEscapeKey);
+  },
+  beforeDestroy() {
+    // Clean up event listener
+    window.removeEventListener('keydown', this.handleEscapeKey);
+  },
   methods: {
-   // none   
+    toggleCard() {
+      this.isOpen = !this.isOpen;
+    },
+    openCard() {
+      this.isOpen = true;
+    },
+    closeCard() {
+      this.isOpen = false;
+    },
+    handleEscapeKey(event) {
+      if (event.key === 'Escape' && this.isOpen) {
+        this.closeCard();
+      }
+    }
   }
 });
 
@@ -41,8 +82,12 @@ Vue.component('hovercard-trigger', {
   template: `
     <button
       type="button"
-      class=" relative"
-      @click="hovercard.tmp"
+      class="relative"
+      @click="hovercard.toggleCard"
+      @focus="hovercard.openCard"
+      @blur="hovercard.closeCard"
+      @mouseenter="hovercard.openCard"
+      @mouseleave="hovercard.closeCard"
     >
       <slot></slot>
     </button>
@@ -54,32 +99,100 @@ Vue.component('hovercard-trigger', {
 Vue.component('hovercard-content', {
   template: `
     <div
-      class="
-            absolute
-            left-1/2
-            -translate-x-1/2
-            w-[200px]
-            hidden
-            fade-in
-            group-hover:block
-            bg-white
-            border
-            border-[--gray]
-            p-4
-            rounded-[--big-radius]
-            shadow-md
-            top-full
-            dark:shadow-none
-            dark:bg-darkest
-            dark:text-white 
-            dark:border-slate-600"
+      v-show="hovercard.isOpen"
+      :class="positionClasses"
+      :style="positionStyles"
+      @click.stop
+      tabindex="-1"
     >
-         <slot></slot>
-       
+      <slot></slot>
     </div>
   `,
-  inject: ['hovercard']
+  inject: ['hovercard'],
+  computed: {
+    positionClasses() {
+      const position = this.hovercard.position;
+      const align = this.hovercard.align;
+
+      const baseClasses = `
+        absolute
+        fade-in
+        bg-white
+        border
+        border-[--gray]
+        p-4
+        z-20
+        rounded-[--big-radius]
+        shadow-md
+        dark:shadow-none
+        dark:bg-darkest
+        dark:text-white
+        dark:border-slate-600
+      `;
+
+      // Position classes
+      let positionClass = '';
+      if (position === 'top') {
+        positionClass = 'bottom-full';
+      } else if (position === 'bottom') {
+        positionClass = 'top-full';
+      } else if (position === 'left') {
+        positionClass = 'right-full';
+      } else if (position === 'right') {
+        positionClass = 'left-full';
+      }
+
+      // Alignment classes
+      let alignClass = '';
+      if (position === 'top' || position === 'bottom') {
+        if (align === 'start') {
+          alignClass = 'left-0';
+        } else if (align === 'center') {
+          alignClass = 'left-1/2 -translate-x-1/2';
+        } else if (align === 'end') {
+          alignClass = 'right-0';
+        }
+      } else if (position === 'left' || position === 'right') {
+        if (align === 'start') {
+          alignClass = 'top-0';
+        } else if (align === 'center') {
+          alignClass = 'top-1/2 -translate-y-1/2';
+        } else if (align === 'end') {
+          alignClass = 'bottom-0';
+        }
+      }
+
+      // Width class - only apply fixed width for top/bottom positions
+      const widthClass = (position === 'top' || position === 'bottom') ? 'w-[200px]' : '';
+
+      return `${baseClasses} ${positionClass} ${alignClass} ${widthClass}`;
+    },
+    positionStyles() {
+      const position = this.hovercard.position;
+      const gap = this.hovercard.gap;
+      const offset = this.hovercard.offset;
+
+      let styles = {};
+
+      // Apply gap
+      if (position === 'top') {
+        styles.marginBottom = gap;
+      } else if (position === 'bottom') {
+        styles.marginTop = gap;
+      } else if (position === 'left') {
+        styles.marginRight = gap;
+      } else if (position === 'right') {
+        styles.marginLeft = gap;
+      }
+
+      // Apply offset
+      if (position === 'top' || position === 'bottom') {
+        styles.marginLeft = offset;
+      } else if (position === 'left' || position === 'right') {
+        styles.marginTop = offset;
+      }
+
+      return styles;
+    }
+  }
 });
-
-
-
--- resources/assets/js/popover.js
+++ resources/assets/js/popover.js
@@ -1,19 +1,18 @@
 /*
 |---------------------------------------------------------------
-| Popover  component
+| Popover component
 |---------------------------------------------------------------
 |
 |
 | @author: IgnitedCMS
 | @license: MIT
-| @version: 1.0
+| @version: 1.1
 | @since: 1.0
 |
 */
 
 // Popover component (parent)
 Vue.component('popover', {
-  props: ['width','halfwidth'],
   template: `
     <div class="popover-container relative" v-click-outside="away">
       <slot></slot>
@@ -24,6 +23,26 @@ Vue.component('popover', {
       popover: this
     };
   },
+  props: {
+    position: {
+      type: String,
+      default: 'bottom',
+      validator: value => ['top', 'bottom', 'left', 'right'].includes(value)
+    },
+    align: {
+      type: String,
+      default: 'center',
+      validator: value => ['start', 'center', 'end'].includes(value)
+    },
+    offset: {
+      type: String,
+      default: '0px'
+    },
+    gap: {
+      type: String,
+      default: '0px'
+    }
+  },
   data() {
     return {
       isOpen: false,
@@ -32,8 +51,8 @@ Vue.component('popover', {
       uniqueId: Math.random().toString(36).substring(2)
     };
   },
+  // No created/beforeDestroy hooks needed
   methods: {
-     
     away() {
       this.show = false;
       this.arr = 'false';
@@ -76,25 +95,10 @@ Vue.component('popover-content', {
   template: `
     <div
       :id="'popover-' + popover.uniqueId"
-      class="
-        fade-in-bottom 
-        absolute
-        bg-white
-        p-4
-        bottom-[40px] 
-        left-[50%]
-        rounded-[--small-radius]
-        border
-        border-[--gray]
-        z-10
-        shadow-md
-        dark:shadow-none
-        dark:bg-darkest
-        dark:text-white 
-        dark:border-slate-600"
+      :class="positionClasses"
+      :style="positionStyles"
       role="dialog"
       v-if="popover.show"
-      :style="{ width: popover.width || '200px', marginLeft : popover.halfwidth }"
       @click.stop
     >
     <focus-trap :active="popover.show">
@@ -102,6 +106,91 @@ Vue.component('popover-content', {
     </focus-trap>
     </div>
   `,
-  inject: ['popover']
-});
+  inject: ['popover'],
+  computed: {
+    positionClasses() {
+      const position = this.popover.position;
+      const align = this.popover.align;
 
+      const baseClasses = `
+        fade-in
+        absolute
+        bg-white
+        p-4
+        rounded-[--small-radius]
+        border
+        border-[--gray]
+        z-10
+        shadow-md
+        dark:shadow-none
+        dark:bg-darkest
+        dark:text-white
+        dark:border-slate-600
+      `;
+
+      // Position classes
+      let positionClass = '';
+      if (position === 'top') {
+        positionClass = 'bottom-full';
+      } else if (position === 'bottom') {
+        positionClass = 'top-full';
+      } else if (position === 'left') {
+        positionClass = 'right-full';
+      } else if (position === 'right') {
+        positionClass = 'left-full';
+      }
+
+      // Alignment classes
+      let alignClass = '';
+      if (position === 'top' || position === 'bottom') {
+        if (align === 'start') {
+          alignClass = 'left-0';
+        } else if (align === 'center') {
+          alignClass = 'left-1/2 -translate-x-1/2';
+        } else if (align === 'end') {
+          alignClass = 'right-0';
+        }
+      } else if (position === 'left' || position === 'right') {
+        if (align === 'start') {
+          alignClass = 'top-0';
+        } else if (align === 'center') {
+          alignClass = 'top-1/2 -translate-y-1/2';
+        } else if (align === 'end') {
+          alignClass = 'bottom-0';
+        }
+      }
+
+      // Width class - only apply fixed width for top/bottom positions
+      const widthClass = (position === 'top' || position === 'bottom') ? 'w-[200px]' : '';
+
+      return `${baseClasses} ${positionClass} ${alignClass} ${widthClass}`;
+    },
+    positionStyles() {
+      const position = this.popover.position;
+      const gap = this.popover.gap;
+      const offset = this.popover.offset;
+
+      let styles = {};
+
+      // Apply gap
+      if (position === 'top') {
+        styles.marginBottom = gap;
+      } else if (position === 'bottom') {
+        styles.marginTop = gap;
+      } else if (position === 'left') {
+        styles.marginRight = gap;
+      } else if (position === 'right') {
+        styles.marginLeft = gap;
+      }
+
+      // Apply offset
+      if (position === 'top' || position === 'bottom') {
+        styles.marginLeft = offset;
+      } else if (position === 'left' || position === 'right') {
+        styles.marginTop = offset;
+      }
+
+      return styles;
+    }
+  }
+});
